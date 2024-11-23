f05aab33f51f163694bc74f5e64e6a9a44165661
sasha_mat <sashmat123@gmail.com>
2024-11-23 17:03:09 +0500

    1 коммит

--- Assets/Scripts/Customization.cs
+++ Assets/Scripts/Customization.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Customization : MonoBehaviour
 {
-    [SerializeField] private GameObject tipeyes;
-    [SerializeField] private GameObject tipmounth;
-    [SerializeField] private GameObject viboreye1;
-    [SerializeField] private GameObject viboreye2;
-    [SerializeField] private GameObject vibormounth1;
-    [SerializeField] private GameObject vibormounth2;
+    [SerializeField] private Button tipeyes;
+    [SerializeField] private Button tipmounth;
+    [SerializeField] private Button viboreye1;
+    [SerializeField] private Button viboreye2;
+    [SerializeField] private Button vibormounth1;
+    [SerializeField] private Button vibormounth2;
+
+    [SerializeField] private GameObject eye1;
+    [SerializeField] private GameObject eye2;
+    [SerializeField] private GameObject mounth1;
+    [SerializeField] private GameObject mounth2;
 }
