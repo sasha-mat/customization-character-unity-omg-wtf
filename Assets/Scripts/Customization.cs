310d02e58a5f354ccbca7edc1784e5cd7d4d0c7f
Ya_VaFel <sashmat123@gmail.com>
2024-11-23 16:43:21 +0500

    Initial commit

---
+++ Assets/Scripts/Customization.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Customization : MonoBehaviour
+{
+    [SerializeField] private GameObject tipeyes;
+    [SerializeField] private GameObject tipmounth;
+    [SerializeField] private GameObject viboreye1;
+    [SerializeField] private GameObject viboreye2;
+    [SerializeField] private GameObject vibormounth1;
+    [SerializeField] private GameObject vibormounth2;
+}
