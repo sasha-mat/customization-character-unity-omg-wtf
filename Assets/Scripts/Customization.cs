dc2308116720e374b2a43be1491ce4d818d11c30
sasha_mat <sashmat123@gmail.com>
2024-12-07 14:18:30 +0500

    а

--- Assets/Scripts/Customization.cs
+++ Assets/Scripts/Customization.cs
@@ -5,24 +5,32 @@ public class Customization : MonoBehaviour
 {
     [SerializeField] private GameObject choiceeye1;
     [SerializeField] private GameObject choiceeye2;
-    [SerializeField] private GameObject choicemounth1;
-    [SerializeField] private GameObject choicemounth2;
+    [SerializeField] private GameObject choicemouth1;
+    [SerializeField] private GameObject choicemouth2;
     [SerializeField] private GameObject prefabplayer;
     [SerializeField] private GameObject[] eye;
     [SerializeField] private GameObject[] mounth;
+    [SerializeField] private GameObject galochkachoiceeyes;
+    [SerializeField] private GameObject galochkachoicemouths;
+    [SerializeField] private GameObject[] galochkaeye;
+    [SerializeField] private GameObject[] galochkamouth;
 
     public void Eyes()
     {
+        galochkachoiceeyes.SetActive(true);
+        galochkachoicemouths.SetActive(false);
         choiceeye1.SetActive(true);
         choiceeye2.SetActive(true);
-        choicemounth1.SetActive(false);
-        choicemounth2.SetActive(false);
+        choicemouth1.SetActive(false);
+        choicemouth2.SetActive(false);
     }
 
     public void Mounths()
     {
-        choicemounth1.SetActive(true);
-        choicemounth2.SetActive(true);
+        galochkachoiceeyes.SetActive(false);
+        galochkachoicemouths.SetActive(true);
+        choicemouth1.SetActive(true);
+        choicemouth2.SetActive(true);
         choiceeye1.SetActive(false);
         choiceeye2.SetActive(false);
     }
@@ -36,9 +44,32 @@ public class Customization : MonoBehaviour
         select.SetActive(true);
     }
 
+    public void GalochkaEye(GameObject select)
+    {
+        foreach (GameObject go in galochkaeye)
+        {
+            go.SetActive(false);
+        }
+        select.SetActive(true);
+    }
+
     public void ChangeMounth(GameObject select)
     {
         foreach (GameObject go in mounth)
+        {
+            go.SetActive(false);
+        }
+        select.SetActive(true);
+        foreach (GameObject go in galochkamouth)
+        {
+            go.SetActive(false);
+        }
+        select.SetActive(true);
+    }
+
+    public void GalochkaMouth(GameObject select)
+    {
+        foreach (GameObject go in galochkamouth)
         {
             go.SetActive(false);
         }
